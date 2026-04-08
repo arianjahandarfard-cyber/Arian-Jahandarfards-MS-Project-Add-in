49270e0c469709513972e6a2fef782ca82ac45a9
arian <arian@DESKTOP-JAJU6L4>
2026-04-08 13:46:27 -0500

    git tag v2.2.2.20       git push origin v2.2.2.20

--- Arian Jahandarfards MS Project Add-in/Properties/AssemblyInfo.cs
+++ Arian Jahandarfards MS Project Add-in/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.2.18")]
-[assembly: AssemblyFileVersion("2.2.2.18")]
+[assembly: AssemblyVersion("2.2.2.20")]
+[assembly: AssemblyFileVersion("2.2.2.20")]
 
